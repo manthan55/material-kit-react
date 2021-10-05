fb1a2eb413a019e4b6755fdfadf689f0ea1d261f
Manthan Patidar <manthanpatidar@gmail.com>
2021-10-05 14:38:23 +0530

    Adding delete endpoint functionality

--- src/components/applicationHealthCheck/Endpoints.js
+++ src/components/applicationHealthCheck/Endpoints.js
@@ -6,13 +6,16 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import plusFill from '@iconify/icons-eva/plus-fill';
+import { useNavigate } from 'react-router-dom';
 
 // Icons
 import { Icon } from '@iconify/react';
 import { SvgIcon, Card, Box, CardHeader, Stack, Button } from '@mui/material';
 import checkmarkCircleFill from '@iconify/icons-eva/checkmark-circle-2-fill';
 import alertCircleFill from '@iconify/icons-eva/alert-circle-fill';
+import plusFill from '@iconify/icons-eva/plus-fill';
+import deleteIcon from '@iconify/icons-ic/delete';
+import editIcon from '@iconify/icons-ic/edit';
 
 function createData(endpoint, status, response, statusCode, expectedStatusCode, email) {
   return { endpoint, status, response, statusCode, expectedStatusCode, email };
@@ -22,18 +25,29 @@ const rows = [
   // Status
   // true = Running
   // false = Error
-  createData('http://google.com', true, 'some response string', 201, 201, 'team@app1.com'),
-  createData('http://youtube.com', false, 'some response string', 500, 201, 'team@app2.com'),
-  createData('http://facebook.com', true, 'some response string', 200, 200, 'team@app3.com')
+  createData('google', true, 'some response string', 201, 201, 'team@app1.com'),
+  createData('youtube', false, 'some response string', 500, 201, 'team@app2.com'),
+  createData('facebook', true, 'some response string', 200, 200, 'team@app3.com')
 ];
 
 export default function Endpoints() {
+  const navigate = useNavigate();
+
+  const DeleteEndpoint = (endpointName) => {
+    // Make API call to delete the endpoint
+    console.log(endpointName);
+  };
+
   return (
     <Card>
       <Stack direction="row" alignItems="center" justifyContent="space-between">
         <CardHeader title="Endpoints Status" />
         <Box sx={{ pr: 2, pt: 2, pb: 2 }}>
-          <Button variant="contained" to="#" startIcon={<Icon icon={plusFill} />}>
+          <Button
+            variant="contained"
+            href="/dashboard/application-health-check/add-edit-endpoint"
+            startIcon={<Icon icon={plusFill} />}
+          >
             Add Endpoint
           </Button>
         </Box>
@@ -49,6 +63,7 @@ export default function Endpoints() {
                 <TableCell>Status Code</TableCell>
                 <TableCell>Expected Status Code</TableCell>
                 <TableCell>Email</TableCell>
+                <TableCell>Actions</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -75,6 +90,16 @@ export default function Endpoints() {
                   <TableCell>{row.statusCode}</TableCell>
                   <TableCell>{row.expectedStatusCode}</TableCell>
                   <TableCell>{row.email}</TableCell>
+                  <TableCell>
+                    <SvgIcon
+                      sx={{ color: 'red' }}
+                      onClick={() => {
+                        DeleteEndpoint(row.endpoint);
+                      }}
+                    >
+                      <Icon icon={deleteIcon} width={24} height={24} />
+                    </SvgIcon>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
